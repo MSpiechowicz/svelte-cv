a3ce3aaa8499992d646650ee11fff31996df69ee
Maciej Spiechowicz <spm1fr@bosch.com>
2025-08-07 16:36:48 +0200

    Update avatar handling in AppCVHeaderAvatar component to use dynamic avatar URL from cv.json and improve error handling for image loading

--- src/lib/components/AppCVHeaderAvatar.svelte
+++ src/lib/components/AppCVHeaderAvatar.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+	import { header } from '$lib/cv.json';
 	import ManAvatar from '$lib/static/ManAvatar.svelte';
 	import WomanAvatar from '$lib/static/WomanAvatar.svelte';
-	import { header } from '$lib/cv.json';
+	import { handleImageError } from '$lib/utils';
 </script>
 
 <div
 	class="w-28 h-auto bg-avatar-background rounded-full justify-center items-center overflow-hidden hidden lg:flex border-2 border-avatar-ring"
 >
-	{#if header.gender === 'male'}
-		<ManAvatar fillColor="var(--color-avatar)" customClass="w-fit mt-2" />
+	{#if header.avatar}
+		<img
+			src={header.avatar}
+			alt="{header.name} avatar"
+			class="w-full h-full object-cover"
+			onerror={handleImageError}
+		/>
 	{:else}
-		<WomanAvatar fillColor="var(--color-avatar)" customClass="w-fit mt-2" />
+		{#if header.gender === 'male'}
+			<ManAvatar fillColor="var(--color-avatar)" customClass="w-fit mt-2" />
+		{:else}
+			<WomanAvatar fillColor="var(--color-avatar)" customClass="w-fit mt-2" />
+		{/if}
 	{/if}
 </div>
