659d12e647ae3f1f653ab6840c6c7372f21c2f4d
Maciej Spiechowicz <spm1fr@bosch.com>
2025-08-07 14:35:09 +0200

    Replace consent text with AppCVConsent component in AppCV for improved modularity

--- src/lib/components/AppCV.svelte
+++ src/lib/components/AppCV.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
+	import AppCVConsent from './AppCVConsent.svelte';
 	import AppCVHeader from './AppCVHeader.svelte';
 	import AppCVSections from './AppCVSections.svelte';
-
-	const currentDate = new Date();
-	const consentValidUntil = new Date(currentDate.getFullYear(), 11, 31);
 </script>
 
 <div
@@ -11,17 +9,5 @@
 >
 	<AppCVHeader />
 	<AppCVSections />
-	<div class="flex flex-col my-6 items-center justify-center gap-1 px-4">
-		<p class="text-center font-extralight text-xs">
-			I consent to the processing of my personal data for the purposes of current and future
-			recruitment processes.
-		</p>
-		<p class="text-center font-extralight text-xs">
-			This consent is valid until {consentValidUntil.toLocaleDateString('en-US', {
-				month: 'long',
-				day: 'numeric',
-				year: 'numeric'
-			})}.
-		</p>
-	</div>
+	<AppCVConsent />
 </div>
---
+++ src/lib/components/AppCVConsent.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  const currentDate = new Date();
+  const consentValidUntil = new Date(currentDate.getFullYear(), 11, 31);
+</script>
+
+<div class="flex flex-col my-6 items-center justify-center gap-1 px-4">
+  <p class="text-center font-extralight text-xs">
+    I consent to the processing of my personal data for the purposes of current and future
+    recruitment processes.
+  </p>
+  <p class="text-center font-extralight text-xs">
+    This consent is valid until {consentValidUntil.toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric'
+    })}.
+  </p>
+</div>
