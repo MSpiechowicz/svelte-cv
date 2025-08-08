51d0a9e7df809877966bbe521ab574d58831f714
Maciej Spiechowicz <spm1fr@bosch.com>
2025-08-08 08:40:54 +0200

    Update AppCVConsent component to improve text styling for better readability and visual consistency.

--- src/lib/components/AppCVConsent.svelte
+++ src/lib/components/AppCVConsent.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div class="flex flex-col my-6 items-center justify-center gap-1 px-4">
-  <p class="text-center font-extralight text-xs">
+  <p class="text-center text-text-secondary font-extralight text-xs">
     I consent to the processing of my personal data for the purposes of current and future
     recruitment processes.
   </p>
-  <p class="text-center font-extralight text-xs">
+  <p class="text-center text-text-secondary font-extralight text-xs">
     This consent is valid until {consentValidUntil.toLocaleDateString('en-US', {
       month: 'long',
       day: 'numeric',
